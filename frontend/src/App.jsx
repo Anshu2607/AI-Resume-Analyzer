b8ee3613032a1112966727e753b74d522f3da890
Anshu Priya <anshu260304@gmail.com>
2026-04-28 00:21:52 +0530

    Implemented complete frontend routing system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Upload from "./pages/Upload";
+import Analysis from "./pages/Analysis";
 import Interview from "./pages/Interview";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Interview />
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="/upload" element={<Upload />} />
+
+        <Route path="/analysis" element={<Analysis />} />
+
+        <Route path="/interview" element={<Interview />} />
+
+      </Routes>
     </BrowserRouter>
   );
 }
